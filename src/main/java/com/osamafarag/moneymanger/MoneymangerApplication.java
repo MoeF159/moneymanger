36a42ee7dbdfe84c531272dda0c0303a5c3ddb1f
Osama Farag <ofarag115@gmail.com>
2026-04-07 08:12:52 -0400

    Initial commit

---
+++ src/main/java/com/osamafarag/moneymanger/MoneymangerApplication.java
@@ -0,0 +1,13 @@
+package com.osamafarag.moneymanger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoneymangerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoneymangerApplication.class, args);
+	}
+
+}
