368d45c1b876505080e414f9725ee2c824e4d933
Osama Farag <ofarag115@gmail.com>
2026-04-10 20:39:21 -0400

    Changed annotation to resolve LAZY FetchType

--- src/main/java/com/osamafarag/moneymanger/repository/ExpenseRepository.java
+++ src/main/java/com/osamafarag/moneymanger/repository/ExpenseRepository.java
@@ -47,13 +47,13 @@ public interface ExpenseRepository extends JpaRepository<ExpenseEntity, Long>{
     * adding proper access control.
     */
    
-    @Query("""
-    SELECT e 
-    FROM ExpenseEntity e 
-    JOIN FETCH e.category 
-    WHERE e.profile.id = :profileId 
-    AND e.date = :date
-    """)
+    // @Query("""
+    // SELECT e 
+    // FROM ExpenseEntity e 
+    // JOIN FETCH e.category 
+    // WHERE e.profile.id = :profileId 
+    // AND e.date = :date
+    // """)
     List<ExpenseEntity>findByProfileIdAndDate(
         Long profileId, 
         LocalDate date
